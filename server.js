420ffeb2ce68665a82121c3cf318c072cecd245b
Sean Pine <scpine8@gmail.com>
2021-07-14 17:38:42 -0400

    feat: added a POST function and a second GET function

--- server.js
+++ server.js
@@ -7,9 +7,32 @@ const port = 8000;
 app.use(cors());
 app.use(bodyParser());
 
+let data = {
+    info: "Hello World!",
+    names: {
+        "Sean": 10,
+        "Bob": 5,
+        "Steve": 5,
+    }
+}
+
 app.get("/", (req, res) => {
-  res.send(data);
+    res.send(data.info);
 });
+
+app.get("/names", (req, res) => {
+        res.send(data.names);
+    });
+
+app.post("/", (req, res) => {
+    let { user, pass } = req.body;
+
+    if (user == 'hayden' && pass == 'boshart') {
+        res.send("You're In")
+    }
+
+    res.send("Hello World!");
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
